30dc34f99a9db6b50a52b7b79318166685e3d8d1
unknown <rajivkumar.p1994@gmail.com>
2018-07-12 11:25:01 +0530

    Updated Read file with promises

--- readfile_with_promise.js
+++ readfile_with_promise.js
@@ -3,23 +3,28 @@ var promise = require('promise');
 
 console.log('Executed before promise reading');
 
-function readFile(filename, enc){
-    return new Promise((resolve, reject) => {
+//let readPromise = function readFile(filename, enc){
+    let readPromise = new Promise((resolve, reject) => {
         fs.readFile('./files/files', 'utf8', function(err, data){
         if(err){
             console.log('Rejected');
             reject(err);
-            console.log(data);
+            //console.log(data);
         }
         else{
-            resolve(data);
             console.log('Resolved');
-            console.log(data);
+            resolve(data);
+            //console.log(data);
         }
     });
 });
-    
-}
+
+
+readPromise.then(function(fromResolve){
+    console.log('Data is ' + fromResolve);
+}).catch(function(fromReject){
+    console.log('Data is ' + fromReject);
+})
 
 /**fs.readFile('./files/files', 'utf8', function(err, data){
     return new Promise((resolve, reject) => {
