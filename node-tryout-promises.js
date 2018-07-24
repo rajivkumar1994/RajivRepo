ac5e6a6ba0fbb0ab32cbd42ae50b324faece51fc
unknown <rajivkumar.p1994@gmail.com>
2018-07-24 13:04:10 +0530

    Node tryout with promises

---
+++ node-tryout-promises.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var promise = require('promise');
+
+console.log('Executed before promise reading');
+
+
+ let readPromise = fs.readFile('./node-tryout-master/node-tryout-master/error.txt', 'utf8', function(err, data){
+      return new Promise((resolve, reject) => {
+        if(err){
+            console.log('Rejected');
+            reject(err);
+            console.log(data);
+        }
+        else{
+            console.log('Resolved');
+            resolve(data);
+            console.log(data);
+        }
+    });
+
+    readPromise.then(function(fromResolve){
+    console.log('Data is : ' + fromResolve);
+}).catch(function(fromReject){
+    console.log('Data Not Found : ' + fromReject);
+})
+     
+});
+
+console.log('Executed after promise reading');
